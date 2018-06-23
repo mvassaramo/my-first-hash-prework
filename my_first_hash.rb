d073b0974f66bb082a3118d711dc9a728597294c
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-23 15:25:56 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,13 +34,3 @@ def adding
 end
 
  
-
-
- # add 2 muskets to the shipping_manifest hash below
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
