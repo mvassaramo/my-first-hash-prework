37ff0d00287c1ea485caf27e327d1febfe57c4ef
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-06-23 15:18:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,14 +29,15 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+ 
 
-shipping_manifest[oil paintings]
+end
+
+ # add 2 muskets to the shipping_manifest hash below
+
 
   # add 4 gun powder to the shipping_manifest hash below
 
 
 
   # return the shipping_manifest hash below
-
-end
